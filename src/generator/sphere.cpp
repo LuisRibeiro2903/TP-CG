0ab8d4eff8104b65103709c367f47b48a0a2d3db
Samba@X13 <santos.diogo.21@outlook.com>
2024-03-06 12:11:51 +0000

    Comeco da reimplementacao da esfera

--- src/generator/sphere.cpp
+++ src/generator/sphere.cpp
@@ -1,21 +1,50 @@
 #include "point.hpp"
+#include <algorithm>
 #include <cmath>
 #include <vector>
 
 std::vector<Point> generateSphere(float radius, int slices, int stacks) {
   std::vector<Point> points;
 
-  for (int stack = 0; stack <= stacks; ++stack) {
-    float phi = M_PI * stack / stacks;
+  float angle_cur_hor;
+  float angle_next_hor;
+  float angle_step_hor = 360.0f / (float)slices;
+  float angle_cur_ver;
+  float angle_next_ver;
+  float angle_step_ver = 180.0f / (float)stacks;
+  float angle_init_ver = 90.0f - angle_step_ver;
 
-    for (int slice = 0; slice < slices; ++slice) {
-      float theta = 2 * M_PI * slice / slices;
+  for (int i = 0; i < slices; i++) {
+    // calc hor angle
+    angle_cur_hor = i * angle_step_hor;
+    angle_next_hor = (i + 1) * angle_step_hor;
 
-      float x = radius * std::sin(phi) * std::cos(theta);
-      float y = radius * std::cos(phi);
-      float z = radius * std::sin(phi) * std::sin(theta);
+    // draw top
+    Point t1(0.0f, radius, 0.0f);
+    Point t2(radius * cos(angle_cur_hor), radius * sin(angle_init_ver),
+             radius * sin(angle_cur_hor));
+    Point t3(radius * cos(angle_next_hor), radius * sin(angle_init_ver),
+             radius * sin(angle_next_hor));
+    points.push_back(t1);
+    points.push_back(t2);
+    points.push_back(t3);
 
-      points.push_back(Point(x, y, z));
+    // draw bot
+    Point b1(0.0f, radius, 0.0f);
+    Point b2(radius * cos(angle_next_hor), radius * sin(-angle_init_ver),
+             radius * sin(angle_next_hor));
+    Point b3(radius * cos(angle_cur_hor), radius * sin(-angle_init_ver),
+             radius * sin(angle_next_hor));
+    points.push_back(b1);
+    points.push_back(b2);
+    points.push_back(b3);
+
+    // remove top and bot
+    stacks -= 2;
+    if (stacks > 0) {
+      angle_cur_ver = angle_init_ver;
+      for (int i = 0; i < stacks; i++) {
+      }
     }
   }
 

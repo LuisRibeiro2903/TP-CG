6f4a9145bfdd8fce03cedb7e62fd0645f2161347
A100647 <a100647@alunos.uminho.pt>
2024-03-07 17:30:20 +0000

    Cone feito

---
+++ src/generator/cone.cpp
@@ -0,0 +1,42 @@
+#include "point.hpp"
+#include <vector>
+#include <math.h>
+
+std::vector<Point> generateCone(float radius, float height, int slices, int stacks) {
+    std::vector<Point> vertices;
+
+    // Vertice para o "corpo" do cone
+    for (int i = 0; i < slices; i++) {
+        float angle = 2 * M_PI * i / slices;
+        float nextAngle = 2 * M_PI * (i + 1) / slices;
+
+        // Base
+        vertices.emplace_back(0, 0, 0);
+        vertices.emplace_back(radius * cos(angle), 0, radius * sin(angle));
+        vertices.emplace_back(radius * cos(nextAngle), 0, radius * sin(nextAngle));
+
+        // Corpo do cone
+        for (int j = 0; j < stacks; j++) {
+            float currentHeight = height * j / stacks;
+            float nextHeight = height * (j + 1) / stacks;
+            float currentRadius = radius * (1 - (float)j / stacks);
+            float nextRadius = radius * (1 - (float)(j + 1) / stacks);
+
+            Point p1(currentRadius * cos(angle), currentHeight, currentRadius * sin(angle));
+            Point p2(nextRadius * cos(angle), nextHeight, nextRadius * sin(angle));
+            Point p3(currentRadius * cos(nextAngle), currentHeight, currentRadius * sin(nextAngle));
+            Point p4(nextRadius * cos(nextAngle), nextHeight, nextRadius * sin(nextAngle));
+
+            // Triângulos que compõem cada fatia do cone
+            vertices.push_back(p1);
+            vertices.push_back(p2);
+            vertices.push_back(p3);
+
+            vertices.push_back(p3);
+            vertices.push_back(p2);
+            vertices.push_back(p4);
+        }
+    }
+
+    return vertices;
+}

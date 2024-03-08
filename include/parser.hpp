ee3c517519f53a6b4aa02717b90158c2b28ca9e3
Samba@X13 <santos.diogo.21@outlook.com>
2024-03-08 11:54:22 +0000

    Camera

--- include/parser.hpp
+++ include/parser.hpp
@@ -1,31 +1,29 @@
 #ifndef PARSER_CPP
 #define PARSER_CPP
 
-#include <vector>
 #include <array>
+#include <string>
+#include <vector>
 
 #include "point.hpp"
-#include "tinyxml2.h"
 
 using std::vector, std::string;
 
 class ParsedWorld {
-    public:
-        int _windowWidth;
-        int _windowHeight;
-        std::array<Point, 3> _lookAt;
-        std::array<float, 3> _projection;
-        std::vector<std::string> _models;
+public:
+  int _windowWidth;
+  int _windowHeight;
+  std::array<Point, 3> _lookAt;
+  std::array<float, 3> _projection;
+  std::vector<std::string> _models;
 
-        ParsedWorld(std::array<Point, 3>& lookAt,
-                    std::array<float, 3>& projection,
-                    int windowWidth,
-                    int windowHeight,
-                    std::vector<std::string>& models);
+  ParsedWorld(std::array<Point, 3> &lookAt, std::array<float, 3> &projection,
+              int windowWidth, int windowHeight,
+              std::vector<std::string> &models);
 };
 
-vector<vector<Point>> parse3dFile (vector<string> models);
+vector<vector<Point>> parse3dFile(vector<string> models);
 
-ParsedWorld worldParser (const char* filename);
+ParsedWorld worldParser(const char *filename);
 
 #endif

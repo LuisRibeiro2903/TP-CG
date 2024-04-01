74c3d3a10c17603e6a3214ec07395743bf5427fb
Samba@X13 <santos.diogo.21@outlook.com>
2024-04-01 15:39:23 +0100

    Codigo para a 'arvore' das transformacoes\modelo

--- src/engine/groups.cpp
+++ src/engine/groups.cpp
@@ -1,5 +1,13 @@
+#include <model.hpp>
 #include <transform.hpp>
 #include <vector>
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glew.h>
+#include <GL/glut.h>
+#endif
 
 class Model {
   int temp;
@@ -15,4 +23,18 @@ public:
             std::vector<Model> models)
       : sub_nodes(std::move(sub_nodes)), transforms(std::move(transforms)),
         models(std::move(models)) {}
+
+  void drawNodes() {
+    glPushMatrix();
+
+    for (Transform t : transforms) {
+      t.applyTransform();
+    }
+
+    for (Model m : models) {
+      m.drawModel();
+    }
+
+    glPopMatrix();
+  }
 };

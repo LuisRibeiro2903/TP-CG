c40aa4d6ab002eb89a02afe4b32a2eec33726c6d
samba@hows <santos.diogo.21@outlook.com>
2024-05-20 17:45:38 +0100

    Texturas da Box por testar.
    Considerei que o ficheiro de input era uma grelha 3x3 onde a coluna da
    diretira n se usava (ns se o ratio na imagem teria efeito na textura ao
    ser aplicada). As duas da linha de cima sao para as faces do plano XZ,
    as seguintes para o plano XY e as da ultima linha sao para o YZ.

--- include/parsedModel.hpp
+++ include/parsedModel.hpp
@@ -8,20 +8,22 @@
 using namespace std;
 
 class ParsedModel {
-        
-    private:
-        vector<Point> vertex;
-        vector<Point> normals;
-        vector<Point> textures;
-        int size;
 
-    public:
-        ParsedModel(vector<Point> vertex, vector<Point> normals, vector<Point> textures);
+private:
+  vector<Point> vertex;
+  vector<Point> normals;
+  vector<Point> textures;
+  // NOTE: Samba- ns se isto faz sentido exisitir.
+  int size;
 
-        vector<Point> getVertex();
-        vector<Point> getNormals();
-        vector<Point> getTextures();
-        int getSize();
+public:
+  ParsedModel(vector<Point> vertex, vector<Point> normals,
+              vector<Point> textures);
+
+  vector<Point> getVertex();
+  vector<Point> getNormals();
+  vector<Point> getTextures();
+  int getSize();
 };
 
 #endif
--- src/generator/box.cpp
+++ src/generator/box.cpp
@@ -1,192 +1,245 @@
-#include <vector>
-#include <tuple>
-#include "point.hpp"
 #include "parsedModel.hpp"
+#include "point.hpp"
+#include <tuple>
+#include <vector>
 
+void generatePlanesXZ(float dimension, int divisions, vector<Point> &vertices,
+                      vector<Point> &normals, vector<Point> &textures) {
+  float step = dimension / divisions;
+  float step_tex_grid = 1.0f / 3.0f;
+  float step_tex = step_tex_grid / divisions;
+  float halfDimension = dimension / 2.0f;
 
-std::tuple<std::vector<Point>, std::vector<Point>> generatePlanesXZ(float dimension,int divisions)
-{
-    std::vector<Point>vertices;
-    std::vector<Point>normals;
+  for (int i = 0; i < divisions; i++) {
+    for (int j = 0; j < divisions; j++) {
 
-    float step = dimension / divisions;
-    float halfDimension = dimension / 2.0f;
+      // Calculate vertices for the current square
+      float x0 = -halfDimension + j * step;
+      float z0 = -halfDimension + i * step;
+      float x1 = x0 + step;
+      float z1 = z0 + step;
 
-        for (int i = 0; i < divisions; i++) {
-            for (int j = 0; j < divisions; j++) {
-            
-            // Calculate vertices for the current square
-            float x0 = -halfDimension + j * step;
-            float z0 = -halfDimension + i * step;
-            float x1 = x0 + step;
-            float z1 = z0 + step;
+      // Vertices for Top Face
+      float x0_text_top = j * step_tex;
+      float y0_text_top = step_tex_grid * 2.0f + i * step_tex;
+      float x1_text_top = x0_text_top + step_tex;
+      float y1_text_top = y0_text_top + step_tex;
 
-            // Lower triangle Top Face
-            vertices.emplace_back(x0, halfDimension, z0);
-            normals.emplace_back(0, 1, 0);
-            vertices.emplace_back(x1, halfDimension, z1);
-            normals.emplace_back(0, 1, 0);
-            vertices.emplace_back(x1, halfDimension, z0);   
-            normals.emplace_back(0, 1, 0);
+      // Vertices for Bot Face
+      float x0_text_bot = step_tex_grid * j * step_tex;
+      float y0_text_bot = step_tex_grid * 2.0f + i * step_tex;
+      float x1_text_bot = x0_text_bot + step_tex;
+      float y1_text_bot = y0_text_bot + step_tex;
 
-            // Upper triangle Top Face
-            vertices.emplace_back(x0, halfDimension, z1);
-            normals.emplace_back(0, 1, 0);
-            vertices.emplace_back(x1, halfDimension, z1);
-            normals.emplace_back(0, 1, 0);
-            vertices.emplace_back(x0, halfDimension, z0);
-            normals.emplace_back(0, 1, 0);
+      // Lower triangle Top Face - T1
+      vertices.emplace_back(x0, halfDimension, z0);
+      normals.emplace_back(0, 1, 0);
+      textures.emplace_back(x0_text_top, y1_text_top, 0);
+      vertices.emplace_back(x1, halfDimension, z1);
+      normals.emplace_back(0, 1, 0);
+      textures.emplace_back(x1_text_top, y0_text_top, 0);
+      vertices.emplace_back(x1, halfDimension, z0);
+      normals.emplace_back(0, 1, 0);
+      textures.emplace_back(x1_text_top, y1_text_top, 0);
 
-            // Upper triangle Bottom Face
-            vertices.emplace_back(x0, -halfDimension, z0);
-            normals.emplace_back(0, -1, 0);
-            vertices.emplace_back(x1, -halfDimension, z0);  
-            normals.emplace_back(0, -1, 0);
-            vertices.emplace_back(x0, -halfDimension, z1);
-            normals.emplace_back(0, -1, 0);
+      // Upper triangle Top Face - T2
+      vertices.emplace_back(x0, halfDimension, z1);
+      normals.emplace_back(0, 1, 0);
+      textures.emplace_back(x0_text_top, y0_text_top, 0);
+      vertices.emplace_back(x1, halfDimension, z1);
+      normals.emplace_back(0, 1, 0);
+      textures.emplace_back(x1_text_top, y0_text_top, 0);
+      vertices.emplace_back(x0, halfDimension, z0);
+      normals.emplace_back(0, 1, 0);
+      textures.emplace_back(x0_text_top, y1_text_top, 0);
 
-            // Lower triangle Bottom Face
-            vertices.emplace_back(x0, -halfDimension, z1);
-            normals.emplace_back(0, -1, 0);
-            vertices.emplace_back(x1, -halfDimension, z0);
-            normals.emplace_back(0, -1, 0);
-            vertices.emplace_back(x1, -halfDimension, z1);
-            normals.emplace_back(0, -1, 0);
+      // Lower triangle Bottom Face - T3
+      vertices.emplace_back(x0, -halfDimension, z1);
+      normals.emplace_back(0, -1, 0);
+      textures.emplace_back(x0_text_bot, y1_text_bot, 0);
+      vertices.emplace_back(x1, -halfDimension, z0);
+      normals.emplace_back(0, -1, 0);
+      textures.emplace_back(x1_text_bot, y0_text_bot, 0);
+      vertices.emplace_back(x1, -halfDimension, z1);
+      normals.emplace_back(0, -1, 0);
+      textures.emplace_back(x1_text_bot, y1_text_bot, 0);
 
-        }
+      // Upper triangle Bottom Face - T4
+      vertices.emplace_back(x0, -halfDimension, z0);
+      normals.emplace_back(0, -1, 0);
+      textures.emplace_back(x0_text_bot, y0_text_bot, 0);
+      vertices.emplace_back(x1, -halfDimension, z0);
+      normals.emplace_back(0, -1, 0);
+      textures.emplace_back(x1_text_bot, y0_text_bot, 0);
+      vertices.emplace_back(x0, -halfDimension, z1);
+      normals.emplace_back(0, -1, 0);
+      textures.emplace_back(x0_text_bot, y0_text_bot, 0);
     }
-    return make_tuple(vertices, normals);
+  }
 }
 
-std::tuple<std::vector<Point>, std::vector<Point>> generatePlanesXY(float dimension, int divisions){
-    std::vector<Point> vertices;
-    std::vector<Point> normals;
+void generatePlanesXY(float dimension, int divisions, vector<Point> &vertices,
+                      vector<Point> &normals, vector<Point> &textures) {
+  float step = dimension / divisions;
+  float step_tex_grid = 1.0f / 3.0f;
+  float step_tex = step_tex_grid / divisions;
+  float halfDimension = dimension / 2.0f;
 
-    float step = dimension / divisions;
-    float halfDimension = dimension / 2.0f;
+  for (int i = 0; i < divisions; i++) {
+    for (int j = 0; j < divisions; j++) {
 
-    for (int i = 0; i < divisions; i++) {
-        for (int j = 0; j < divisions; j++) {
-            
-            // Calculate vertices for the current square
-            float x0 = -halfDimension + j * step;
-            float y0 = -halfDimension + i * step;
-            float x1 = x0 + step;
-            float y1 = y0 + step;
+      // Calculate vertices for the current square
+      float x0 = -halfDimension + j * step;
+      float y0 = -halfDimension + i * step;
+      float x1 = x0 + step;
+      float y1 = y0 + step;
 
-            // Lower triangle
-            vertices.emplace_back(x0, y0, halfDimension);
-            normals.emplace_back(0, 0, 1);
-            vertices.emplace_back(x1, y0, halfDimension);
-            normals.emplace_back(0, 0, 1);
-            vertices.emplace_back(x0, y1, halfDimension);
-            normals.emplace_back(0, 0, 1);
+      // Vertices for Front Face
+      float x0_text_front = j * step_tex;
+      float y0_text_front = step_tex_grid + i * step_tex;
+      float x1_text_front = x0_text_front + step_tex;
+      float y1_text_front = y0_text_front + step_tex;
 
-            // Upper triangle
-            vertices.emplace_back(x1, y0, halfDimension);
-            normals.emplace_back(0, 0, 1);
-            vertices.emplace_back(x1, y1, halfDimension);
-            normals.emplace_back(0, 0, 1);
-            vertices.emplace_back(x0, y1, halfDimension);
-            normals.emplace_back(0, 0, 1);
+      // Vertices for Back Face
+      float x0_text_back = step_tex_grid * j * step_tex;
+      float y0_text_back = step_tex_grid + i * step_tex;
+      float x1_text_back = x0_text_back + step_tex;
+      float y1_text_back = y0_text_back + step_tex;
 
-            // Lower triangle
-            vertices.emplace_back(x0, y0, -halfDimension);
-            normals.emplace_back(0, 0, -1);
-            vertices.emplace_back(x1, y1, -halfDimension);
-            normals.emplace_back(0, 0, -1);
-            vertices.emplace_back(x1, y0, -halfDimension);
-            normals.emplace_back(0, 0, -1);
+      // Front Upper triangle - T5
+      vertices.emplace_back(x1, y0, halfDimension);
+      normals.emplace_back(0, 0, 1);
+      textures.emplace_back(x1_text_front, y0_text_front, 0);
+      vertices.emplace_back(x1, y1, halfDimension);
+      normals.emplace_back(0, 0, 1);
+      textures.emplace_back(x1_text_front, y0_text_front, 0);
+      vertices.emplace_back(x0, y1, halfDimension);
+      normals.emplace_back(0, 0, 1);
+      textures.emplace_back(x0_text_front, y1_text_front, 0);
 
-            // Upper triangle
-            vertices.emplace_back(x1, y1, -halfDimension);
-            normals.emplace_back(0, 0, -1);
-            vertices.emplace_back(x0, y0, -halfDimension);
-            normals.emplace_back(0, 0, -1);
-            vertices.emplace_back(x0, y1, -halfDimension);
-            normals.emplace_back(0, 0, -1);
-        }
+      // Front Lower triangle - T6
+      vertices.emplace_back(x0, y0, halfDimension);
+      normals.emplace_back(0, 0, 1);
+      textures.emplace_back(x0_text_front, y0_text_front, 0);
+      vertices.emplace_back(x1, y0, halfDimension);
+      normals.emplace_back(0, 0, 1);
+      textures.emplace_back(x1_text_front, y0_text_front, 0);
+      vertices.emplace_back(x0, y1, halfDimension);
+      normals.emplace_back(0, 0, 1);
+      textures.emplace_back(x0_text_front, y1_text_front, 0);
+
+      // Back Upper triangle - T8
+      vertices.emplace_back(x1, y1, -halfDimension);
+      normals.emplace_back(0, 0, -1);
+      textures.emplace_back(x0_text_back, y0_text_back, 0);
+      vertices.emplace_back(x0, y0, -halfDimension);
+      normals.emplace_back(0, 0, -1);
+      textures.emplace_back(x0_text_back, y0_text_back, 0);
+      vertices.emplace_back(x0, y1, -halfDimension);
+      normals.emplace_back(0, 0, -1);
+      textures.emplace_back(x0_text_back, y0_text_back, 0);
+
+      // Back Lower triangle - T7
+      vertices.emplace_back(x0, y0, -halfDimension);
+      normals.emplace_back(0, 0, -1);
+      textures.emplace_back(x0_text_back, y1_text_back, 0);
+      vertices.emplace_back(x1, y1, -halfDimension);
+      normals.emplace_back(0, 0, -1);
+      textures.emplace_back(x1_text_back, y0_text_back, 0);
+      vertices.emplace_back(x1, y0, -halfDimension);
+      normals.emplace_back(0, 0, -1);
+      textures.emplace_back(x1_text_back, y1_text_back, 0);
     }
-
-    return make_tuple(vertices, normals);
+  }
 }
 
-std::tuple<std::vector<Point>, std::vector<Point>> generatePlanesYZ(float dimension, int divisions){
+void generatePlanesYZ(float dimension, int divisions, vector<Point> &vertices,
+                      vector<Point> &normals, vector<Point> &textures) {
+  float step = dimension / divisions;
+  float step_tex_grid = 1.0f / 3.0f;
+  float step_tex = step_tex_grid / divisions;
+  float halfDimension = dimension / 2.0f;
 
-    std::vector<Point> vertices;
-    std::vector<Point> normals;
+  for (int i = 0; i < divisions; i++) {
+    for (int j = 0; j < divisions; j++) {
 
-    float step = dimension / divisions;
-    float halfDimension = dimension / 2.0f;
+      // Calculate vertices for the current square
+      float y0 = -halfDimension + j * step;
+      float z0 = -halfDimension + i * step;
+      float y1 = y0 + step;
+      float z1 = z0 + step;
 
-    for (int i = 0; i < divisions; i++) {
-        for (int j = 0; j < divisions; j++) {
-            
-            // Calculate vertices for the current square
-            float y0 = -halfDimension + j * step;
-            float z0 = -halfDimension + i * step;
-            float y1 = y0 + step;
-            float z1 = z0 + step;
+      // Vertices for Right Face
+      float x0_text_right = j * step_tex;
+      float y0_text_right = i * step_tex;
+      float x1_text_right = x0_text_right + step_tex;
+      float y1_text_right = y0_text_right + step_tex;
 
-            // Lower triangle Front Face
-            vertices.emplace_back(halfDimension, y0, z0);
-            normals.emplace_back(1, 0, 0);
-            vertices.emplace_back(halfDimension, y1, z0);
-            normals.emplace_back(1, 0, 0);
-            vertices.emplace_back(halfDimension, y1, z1);
-            normals.emplace_back(1, 0, 0);
+      // Vertices for Left Face
+      float x0_text_left = step_tex_grid * j * step_tex;
+      float y0_text_left = i * step_tex;
+      float x1_text_left = x0_text_left + step_tex;
+      float y1_text_left = y0_text_left + step_tex;
 
-            // Upper triangle Front Face
-            vertices.emplace_back(halfDimension, y0, z0);
-            normals.emplace_back(1, 0, 0);
-            vertices.emplace_back(halfDimension, y1, z1);
-            normals.emplace_back(1, 0, 0);
-            vertices.emplace_back(halfDimension, y0, z1);
-            normals.emplace_back(1, 0, 0);
+      // Upper triangle Right Face - T9
+      vertices.emplace_back(halfDimension, y0, z0);
+      normals.emplace_back(1, 0, 0);
+      textures.emplace_back(x1_text_right, y0_text_right, 0);
+      vertices.emplace_back(halfDimension, y1, z0);
+      normals.emplace_back(1, 0, 0);
+      textures.emplace_back(x1_text_right, y1_text_right, 0);
+      vertices.emplace_back(halfDimension, y1, z1);
+      normals.emplace_back(1, 0, 0);
+      textures.emplace_back();
+      textures.emplace_back(x0_text_right, y1_text_right, 0);
 
-            // Lower triangle Back Face
-            vertices.emplace_back(-halfDimension, y0, z0);
-            normals.emplace_back(-1, 0, 0);
-            vertices.emplace_back(-halfDimension, y0, z1);
-            normals.emplace_back(-1, 0, 0);
-            vertices.emplace_back(-halfDimension, y1, z0);
-            normals.emplace_back(-1, 0, 0);
+      // Lower triangle Right Face - T10
+      vertices.emplace_back(halfDimension, y0, z0);
+      normals.emplace_back(1, 0, 0);
+      textures.emplace_back(x1_text_right, y0_text_right, 0);
+      vertices.emplace_back(halfDimension, y1, z1);
+      normals.emplace_back(1, 0, 0);
+      textures.emplace_back(x0_text_right, y1_text_right, 0);
+      vertices.emplace_back(halfDimension, y0, z1);
+      normals.emplace_back(1, 0, 0);
+      textures.emplace_back(x0_text_right, y0_text_right, 0);
 
-            // Upper triangle Back Face
-            vertices.emplace_back(-halfDimension, y1, z1);
-            normals.emplace_back(-1, 0, 0);
-            vertices.emplace_back(-halfDimension, y1, z0);
-            normals.emplace_back(-1, 0, 0);
-            vertices.emplace_back(-halfDimension, y0, z1);
-            normals.emplace_back(-1, 0, 0);
+      // Lower triangle Left Face
+      vertices.emplace_back(-halfDimension, y0, z0);
+      normals.emplace_back(-1, 0, 0);
+      textures.emplace_back(x1_text_left, y0_text_left, 0);
+      vertices.emplace_back(-halfDimension, y0, z1);
+      normals.emplace_back(-1, 0, 0);
+      textures.emplace_back(x0_text_left, y0_text_left, 0);
+      vertices.emplace_back(-halfDimension, y1, z0);
+      normals.emplace_back(-1, 0, 0);
+      textures.emplace_back(x1_text_left, y1_text_left, 0);
 
-            
-        }
+      // Upper triangle Left Face
+      vertices.emplace_back(-halfDimension, y1, z1);
+      normals.emplace_back(-1, 0, 0);
+      textures.emplace_back(x0_text_left, y1_text_left, 0);
+      vertices.emplace_back(-halfDimension, y1, z0);
+      normals.emplace_back(-1, 0, 0);
+      textures.emplace_back(x1_text_left, y1_text_left, 0);
+      vertices.emplace_back(-halfDimension, y0, z1);
+      normals.emplace_back(-1, 0, 0);
+      textures.emplace_back(x0_text_left, y0_text_left, 0);
     }
-
-    return make_tuple(vertices, normals);
+  }
 }
 
-ParsedModel generateBox(float dimension, int divisions){
-    std::vector<Point>box;
-    std::vector<Point>normals;
+ParsedModel generateBox(float dimension, int divisions) {
+  std::vector<Point> box;
+  std::vector<Point> normals;
+  vector<Point> textures;
 
+  // Topo e base
+  generatePlanesXZ(dimension, divisions, box, normals, textures);
+  // Lados
+  generatePlanesYZ(dimension, divisions, box, normals, textures);
+  // Frente e trás
+  generatePlanesXY(dimension, divisions, box, normals, textures);
 
-    //Topo e base
-    auto XZFaces = generatePlanesXZ(dimension, divisions);
-
-    box.insert(box.end(), get<0>(XZFaces).begin(), get<0>(XZFaces).end()); 
-    normals.insert(normals.end(), get<1>(XZFaces).begin(), get<1>(XZFaces).end());
-
-    //Lados
-    auto YZFaces = generatePlanesYZ(dimension, divisions);
-    box.insert(box.end(), get<0>(YZFaces).begin(), get<0>(YZFaces).end());
-    normals.insert(normals.end(), get<1>(YZFaces).begin(), get<1>(YZFaces).end());
-
-    // Frente e trás
-    auto XYFaces = generatePlanesXY(dimension, divisions);
-    box.insert(box.end(), get<0>(XYFaces).begin(), get<0>(XYFaces).end());
-    normals.insert(normals.end(), get<1>(XYFaces).begin(), get<1>(XYFaces).end());
-
-    return ParsedModel(box, normals, {});
+  return ParsedModel(box, normals, textures);
 }
